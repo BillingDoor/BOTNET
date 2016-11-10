4ae3b0972e28c72992b45dd7c0cf0ce87e7d69d6
Loopo <Loopo@10.220.81.250>
2016-11-10 17:41:52 +0100

    aggiunta csfr java

---
+++ src/main/java/cs/sii/security/CsrfSecurityRequestMatcher.java
@@ -0,0 +1,25 @@
+package cs.sii.security;
+
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
+
+/**
+ */
+public class CsrfSecurityRequestMatcher implements RequestMatcher {
+    private Pattern allowedMethods = Pattern.compile("^(HEAD|TRACE|OPTIONS)$");
+
+
+    private RegexRequestMatcher matchUnprotected = new RegexRequestMatcher("/unprotected", null);
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        if(allowedMethods.matcher(request.getMethod()).matches()){
+            return false;
+        }
+
+
+        return (!matchUnprotected.matches(request));
+    }
+}
