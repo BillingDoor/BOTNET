e9987edb1bff2130eb513d0660e7ea55e80a757f
Loopo <Loopo@DESKTOP-2I4KM50>
2016-11-16 15:24:55 +0100

    ll

--- src/main/java/cs/sii/security/WebSecurityConfig.java
+++ src/main/java/cs/sii/security/WebSecurityConfig.java
@@ -44,12 +44,15 @@ protected void configure(HttpSecurity http) throws Exception {
 	http.csrf().disable(); 
 	
 /*
- 	http.authorizeRequests().antMatchers("/newuser/**", "/delete-user-*").permitAll().antMatchers("/edit-user-*")
- 
-			.permitAll().and().formLogin().loginPage("/login").and().csrf().requireCsrfProtectionMatcher(new CsrfSecurityRequestMatcher());
-			.loginProcessingUrl("/login").usernameParameter("ssoId").passwordParameter("password").and()
-			.rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository)
-			.tokenValiditySeconds(86400).and().csrf().and().exceptionHandling().accessDeniedPage("/Access_Denied");
+ @Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/list")
+				.access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
+				.antMatchers("/newuser/**", "/delete-user-*").access("hasRole('ADMIN')").antMatchers("/edit-user-*")
+				.access("hasRole('ADMIN') or hasRole('DBA')").and().formLogin().loginPage("/login")
+				.loginProcessingUrl("/login").usernameParameter("ssoId").passwordParameter("password").and()
+				.rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository)
+				.tokenValiditySeconds(86400).and().csrf().and().exceptionHandling().accessDeniedPage("/Access_Denied");
 */
 }
 
