6df8d376a60fba0772fce6fe1a3c31d1c1a46aa6
xXCiccioXx <christiansimolo@live.com>
2016-12-07 14:45:25 +0100

    sdadsasda

--- src/main/java/cs/sii/service/connection/ConnectionServiceConfig.java
+++ src/main/java/cs/sii/service/connection/ConnectionServiceConfig.java
@@ -9,10 +9,13 @@ import java.util.List;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,8 +34,7 @@ public class ConnectionServiceConfig {
 	
 	@Bean
 	public HttpComponentsClientHttpRequestFactory HttpRequestFactory() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-
-//		
+		
 //		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 //
 //		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
@@ -46,18 +48,24 @@ public class ConnectionServiceConfig {
 //		        .build();
 
 		
+		
+		  SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build());
 
-		System.out.println("bean factory");
+	      HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+
+
+//	      ((HttpComponentsClientHttpRequestFactory) template.getRequestFactory()).setHttpClient(httpClient);
+		
+		
 		HttpComponentsClientHttpRequestFactory crf = new HttpComponentsClientHttpRequestFactory();
 	
-//		crf.setHttpClient(httpClient);
+		crf.setHttpClient(httpClient);
 		
 		crf.setConnectTimeout(configEngine.getConnectTimeout());
 		crf.setConnectionRequestTimeout(configEngine.getRequestTimeout());
 		crf.setReadTimeout(configEngine.getReadTimeout());
 		
 		
-		System.out.println("crf  "+crf.hashCode());
 		
 		
 		
@@ -74,7 +82,6 @@ public class ConnectionServiceConfig {
 
 		MappingJackson2HttpMessageConverter mc = new MappingJackson2HttpMessageConverter();
 		mc.setSupportedMediaTypes(mediaTypes);
-System.out.println("hhhh   "+HttpRequestFactory().hashCode());
 		restTemplate.getMessageConverters().add(mc);
 		return restTemplate;
 	}
