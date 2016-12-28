a8a9a302b4afe8f27e9da90adb57c13e9ce8d1c7
xXCiccioXx <christiansimolo@live.com>
2016-12-28 19:44:26 +0100

    poiuytrew

--- src/main/java/cs/sii/model/bot/Bot.java
+++ src/main/java/cs/sii/model/bot/Bot.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.apache.commons.codec.binary.Base64;
+import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -49,6 +50,7 @@ public class Bot implements Serializable {
 	private String usernameOS;
 	@NotEmpty
 	@Column(name = "elegible", nullable = false)
+	@Type(type="org.hibernate.type.NumericBooleanType")
 	private boolean elegible;
 	
 	@NotEmpty
